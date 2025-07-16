12bd906ad5d60f2abfcd6426d5eefb694ba01538
EkaterineGorgadze <ekaterinegorgadze959@gmail.com>
2025-07-16 11:45:14 +0400

    feat: Recovered and re-added core banking app classes with date input

---
+++ Customer.cs
@@ -0,0 +1,34 @@
+namespace BankingApp
+{
+     public class Customer
+     {
+          public Guid CustomerId { get; private set; } = Guid.NewGuid();
+          public string FullName { get; private set; }
+          public string EmailAddress { get; private set; }
+          public DateTime DateOfBirth { get; private set; }
+
+          public Customer(string name, string email, DateTime birthday)
+          {
+               //CustomerId = id;
+               FullName = name;
+               EmailAddress = email;
+               DateOfBirth = birthday;
+          }
+
+          public string getCustomerSummary()
+          {
+               return $"Customer Information Summary: \n" +
+                      $"Customer Id - {CustomerId}\n" + $"Customer Name - {FullName}\n" +
+                      $"Customer Email - {EmailAddress}\n" + $"Customer Birthdate - {DateOfBirth:dd-MM-yyyy}";
+          }
+
+          public int getAge()
+          {
+               var today = DateTime.Today;
+               var age = today.Year - DateOfBirth.Year;
+               if (DateOfBirth.Date > today.Date) age--;
+               return age;
+          }
+
+     }
+}
